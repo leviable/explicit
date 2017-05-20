debbe53ce74f1c702255b88e9c6246609bbe21f9
levi-rs <levi.noecker@rewardstyle.com>
2017-05-20 08:25:08 -0500

    Move classifiers back to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,24 @@ from setuptools import setup
 
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
     pbr=True,
 )
